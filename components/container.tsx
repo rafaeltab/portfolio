564472fe062a5b677b323f256f0a311de67e5aa4
Rafael <ikbenrafaelbieze2001@gmail.com>
2023-06-28 00:32:13 +0200

    nav bar

---
+++ components/container.tsx
@@ -0,0 +1,14 @@
+const sizes = {
+	"nav": "mx-8 xl:mx-48 lg:mx-32",
+	"body": "xl:mx-80 lg:mx-48",
+} as const;
+
+export function Container(props: React.PropsWithChildren & {
+	size: keyof typeof sizes
+}) {
+	return (
+		<div className={`h-16 ${sizes[props.size]}`} >
+			{props.children}
+		</div>
+	);
+}

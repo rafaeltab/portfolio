4633893608cbe70bfd53e68777cfd17313a1d732
Rafael Bieze <methylium_rafael.bieze@citizenm.com>
2025-10-28 15:15:33 +0000

    Add feature flags using launch darkly

---
+++ src/types/flagSet.ts
@@ -0,0 +1,13 @@
+import { LDFlagSet, useFlags } from "launchdarkly-react-client-sdk";
+
+export const useFeatureFlags = useFlags<FlagSet>;
+export type FlagSet = MustExtend<
+  {
+    techPage?: boolean;
+    timelinePage?: boolean;
+    projectsPage?: boolean;
+  },
+  LDFlagSet
+>;
+
+export type MustExtend<A, T> = A extends T ? A : never;
